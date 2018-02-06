f9f3b71b128e9dbd7794470d275aec83997fa712
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-06 17:21:08 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of(arguments)
     array = []
-    arguments.each {|argument|
+    arguments.each {|*argument|
       self.each {|key, value|
         if value == argument
           array.push(key)
